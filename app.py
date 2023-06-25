8e93319dca1831266d3f2cc856cfe68b7fc877e4
Isidora <vulic.isi@gmail.com>
2023-06-25 12:42:43 +0200

    Added update form for potholes, updated admin and user pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import math
@@ -12,6 +12,10 @@ from datetime import datetime
 import requests
 import json
 import ast
+import secrets
+from flask_session import Session
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///potholes.db' #postavljanje baze
@@ -38,11 +42,18 @@ class Pothole(db.Model):
     def __repr__(self):
         return '<Pothole %r>' % self.id
     
+secret_key = secrets.token_hex(16)
+app.secret_key = secret_key    
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
 with app.app_context():
     db.create_all()
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -52,15 +63,26 @@ def login():
         
         for user in users:
             if user.username == username and user.password == password:
+             
                 if user.role == "admin":
+                    session['username'] = username
                     return redirect('/adminpage')
                 else:
-                    return redirect('/adminpage')
+                    session['username'] = username
+                    return redirect('/userpage')
         
         return render_template('login.html', error='Invalid username or password')
     
     else:
         return render_template('login.html', form_id="login-form")
+    
+@app.route('/logout')
+def logout():
+    # Clear the session data
+    session.clear()
+
+    # Redirect the user to the login page or any other desired page
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -151,54 +173,49 @@ def adminpage():
         # pstr = "Mild"
         # new_pothole = Pothole(longitude = plong, latitude = plat, accel = paccel, strength = pstr)
 
-        with open('server/accelerometer.txt', 'r') as file1, open('server/gyroscope.txt', 'r') as file2, open('server/location.txt', 'r') as file3: 
-            for line1, line2, line3 in zip(file1, file2, file3): 
+        potholes_to_add = []
+
+        with open('server/accelerometer.txt', 'r') as file1, open('server/gyroscope.txt', 'r') as file2, open('server/location.txt', 'r') as file3:
+            for line1, line2, line3 in zip(file1, file2, file3):
                 adict = ast.literal_eval(line1)
                 gdict = ast.literal_eval(line2)
                 ldict = ast.literal_eval(line3)
 
-                if abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])) > 1.5 and abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])) < 3:
-                    print(abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])))
-                    accel = round(math.sqrt(float(adict['z'])**2 + float(adict['x'])**2 +float(adict['y'])**2) *0.1)
-                    long = float(ldict['longitude'])
-                    lat = float(ldict['latitude'])
-                
-                    new_pothole = Pothole(strength = 'Mild', accel = accel, longitude = long, latitude = lat)
-                    try:
-                        db.session.add(new_pothole)
-                        db.session.commit()
-                        return redirect('/')
-                    except:
-                        return "There was an issue adding your pothole"
+                total_acceleration = abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z']))
 
-                elif abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])) >= 3 and abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])) < 6:
-                    print(abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])))
-                    accel = round(math.sqrt(float(adict['z'])**2 + float(adict['x'])**2 +float(adict['y'])**2) *0.1)
+                if 1.5 < total_acceleration < 3:
+                    accel = round(math.sqrt(float(adict['z'])**2 + float(adict['x'])**2 + float(adict['y'])**2) * 0.1)
                     long = float(ldict['longitude'])
                     lat = float(ldict['latitude'])
-                
-                    new_pothole = Pothole(strength = 'Intermediate', accel = accel, longitude = long, latitude = lat)
-                    try:
-                        db.session.add(new_pothole)
-                        db.session.commit()
-                        return redirect('/')
-                    except:
-                        return "There was an issue adding your pothole"
-                elif abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])) >=6:
-                    print(abs(float(gdict['x'])) + abs(float(gdict['y'])) + abs(float(gdict['z'])))
-                    accel = round(math.sqrt(float(adict['z'])**2 + float(adict['x'])**2 +float(adict['y'])**2) *0.1)
+
+                    new_pothole = Pothole(strength='Mild', accel=accel, longitude=long, latitude=lat)
+                    potholes_to_add.append(new_pothole)
+                elif 3 <= total_acceleration < 6:
+                    accel = round(math.sqrt(float(adict['z'])**2 + float(adict['x'])**2 + float(adict['y'])**2) * 0.1)
                     long = float(ldict['longitude'])
                     lat = float(ldict['latitude'])
-                
-                    new_pothole = Pothole(strength = 'Severe', accel = accel, longitude = long, latitude = lat)
-                    try:
-                        db.session.add(new_pothole)
-                        db.session.commit()
-                        return redirect('/')
-                    except:
-                        return "There was an issue adding your pothole"
+
+                    new_pothole = Pothole(strength='Intermediate', accel=accel, longitude=long, latitude=lat)
+                    potholes_to_add.append(new_pothole)
+                elif total_acceleration >= 6:
+                    accel = round(math.sqrt(float(adict['z'])**2 + float(adict['x'])**2 + float(adict['y'])**2) * 0.1)
+                    long = float(ldict['longitude'])
+                    lat = float(ldict['latitude'])
+
+                    new_pothole = Pothole(strength='Severe', accel=accel, longitude=long, latitude=lat)
+                    potholes_to_add.append(new_pothole)
                 else:
                     pass
+
+        for pothole in potholes_to_add:
+            try:
+                db.session.add(pothole)
+
+            except:
+                return "There was an issue adding your pothole"
+
+        db.session.commit()
+        return redirect('/adminpage')
     else:
         potholes = Pothole.query.order_by(Pothole.strength).all()
         return render_template('adminpage.html', potholes = potholes)
@@ -222,11 +239,11 @@ def update(id):
      if request.method == 'POST':
          pothole_to_update.longitude = request.form['longitude']
          pothole_to_update.latitude = request.form['latitude']
-         pothole_to_update.velocity = request.form['velocity']
+         pothole_to_update.accel = request.form['velocity']
          pothole_to_update.strength = request.form['strength']
          try:
              db.session.commit()
-             return redirect('/')
+             return redirect('/adminpage')
          except:
              return 'There was an issue updating your pothole'
      else:
